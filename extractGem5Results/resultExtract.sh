d4d38567027f615be76cb83a6d861eca533ceeee
Ashok Sathyan <ashoksathyan@ee.iitb.ac.in>
2019-05-19 11:09:20 +0530

    Now only gets the last value

--- extractGem5Results/resultExtract.sh
+++ extractGem5Results/resultExtract.sh
@@ -3,20 +3,38 @@
 # This script will extract all the desired parameters from all the benchmarks
 # in the result folder and produce a csv file with all the needed results.
 
-benchmarks=$(ls results)
+if [ ! -d $1 ] 
+then
+    echo "Give results folder as argument"
+    exit 1
+fi
+
+
+folder=$1
+
+benchmarks=$(ls $folder)
 
 parametersToExtract=$(cat parameters)
 
+# Printing the first coloumn
+printf "benchmark, "
+for parameter in $parametersToExtract
+do
+    printf "%s, " "$parameter" 
+done
+printf "\n"
+
 for benchmark in $benchmarks
 do
-    printf "%s,"  "$benchmark"
-	file=results/$benchmark/stats.txt
+    printf "%s, "  "$benchmark"
+	file=$folder/$benchmark/stats.txt
 	for parameter in $parametersToExtract 
 	do
-		#echo $benchmark " " $parameter
-		#grep  $parameter $file | awk -F"/" '{print $1,$2}'|awk '{print $2}' 
-		line=$(grep  $parameter $file | awk -F"/" '{print $1,$2}' | awk '{print $2}')
-        printf "%s," "$line"
+        # tail is being used to filter out the initial garbage results
+		line=$(grep  $parameter $file |tail -n1 |   awk '{print $2}')
+        printf "%s, " "$line"
 	done
     printf "\n"
 done
+
+exit 0
